bf4e6ca95fef2fe44947e62452548df1717801e3
Roman Hizatulin <Roman.Hizatulin@medhost.com>
2019-07-05 20:13:56 +0300

    Added filter by tags

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 /* global stations */
 
-const allStations = stations ? stations : [];
-
-const buttons = {
+const ALL_STATIONS = stations ? stations : [];
+const CONST_BUTTON = {
     play: "▶",
     playPause: "⏯",
     stop: "⏹",
@@ -10,13 +9,17 @@ const buttons = {
     prev: "⏮",
     next: "⏭"
 };
-
-const appStorage = new AppStorage();
+const CONST_TAG = {
+    activeColor: "aquamarine",
+    inactiveColor: "honeydew"
+};
+const APP_STORAGE = new AppStorage();
 let radio = null;
 
 function AppStorage() {
     const defaultStorageObj = {
-        "favorite": {}
+        favorite: [],
+        selectedTag: []
     };
     const localStorageAppName = "rtpp_app_state";
 
@@ -31,29 +34,122 @@ function AppStorage() {
     };
 }
 
-function init() {
+function onLoadBody() {
     window.addEventListener("unhandledrejection", promiseRejectionEvent => {
         console.log(promiseRejectionEvent);
     });
-    const stationContainer = document.getElementById('station_container');
-    let stationCount = 0;
-    let bootstrapElements = "";
 
-    allStations.forEach(station => {
-        bootstrapElements += createStationElement(station);
-        stationCount++;
+    let stationCount = 0;
+    let stationElements = "";
+    let tags = [];
+
+    ALL_STATIONS.forEach(station => {
+        station.tags.forEach(tag => {
+            if (!tags.includes(tag)) {
+                tags.push(tag);
+            }
+        });
+        if (isAnyTagSelected(station.tags)) {
+            stationElements += createStationElement(station);
+            stationCount++;
+        }
+    });
+
+    const stationContainer = document.getElementById('station_container');
+    stationContainer.innerHTML += stationElements;
+
+    tags.sort();
+    let tagElements = "";
+
+    tags.forEach(tag => {
+        tagElements += createTagElement(tag);
+    });
+    const tagContainer = document.getElementById('tag_container');
+    tagContainer.innerHTML += tagElements;
+
+    $("#current_station_name").text(stationCount);
+
+    radio = new Radio(ALL_STATIONS);
+}
+
+function isAnyTagSelected(tags) {
+    const appState = APP_STORAGE.getState();
+    const selectedTags = appState["selectedTag"];
+    let result = false;
+    if (selectedTags.length === 0) {
+        result = true;
+    } else {
+        tags.forEach(tag => {
+            if (selectedTags.includes(tag)) {
+                result = true;
+                return;
+            }
+        });
+    }
+    return result;
+}
+
+function onClickTag(event) {
+    const appState = APP_STORAGE.getState();
+    const tagElement = $(event.currentTarget);
+    const tagName = tagElement.text();
+    let selectedTags = appState["selectedTag"];
+
+    if (selectedTags.includes(tagName)) {
+        // remove element
+        selectedTags.splice(selectedTags.indexOf(tagName), 1);
+        tagElement.css("background-color", CONST_TAG.inactiveColor);
+    } else {
+        selectedTags.push(tagName);
+        tagElement.css("background-color", CONST_TAG.activeColor);
+    }
+    appState["selectedTag"] = selectedTags;
+    APP_STORAGE.setState(appState);
+
+    let stationCount = 0;
+    $("#station_container > .station").each(function () {
+        const elemId = $(this).attr('id').replace("station_element_id_", "");
+        ALL_STATIONS.forEach(station => {
+            if (station.id.toString() === elemId.toString()) {
+                let anyTagFound = false;
+                station.tags.forEach(tag => {
+                    if (selectedTags.includes(tag)) {
+                        anyTagFound = true;
+                        return;
+                    }
+                });
+                if (anyTagFound || selectedTags.length === 0) {
+                    $(this).css('display', 'inline-block');
+                    stationCount++;
+                } else {
+                    $(this).css('display', 'none');
+                }
+                return;
+            }
+        });
     });
     $("#current_station_name").text(stationCount);
-    stationContainer.innerHTML += bootstrapElements;
-
-    radio = new Radio(allStations);
 }
-;
+
+function createTagElement(tagName) {
+    const appState = APP_STORAGE.getState();
+    let tagColor = CONST_TAG.inactiveColor;
+    if (appState["selectedTag"].includes(tagName)) {
+        tagColor = CONST_TAG.activeColor;
+    } else {
+        tagColor = CONST_TAG.inactiveColor;
+    }
+    const result = '' +
+            '<div tabIndex="0" class="tag" onclick="onClickTag(event);" style="background-color: ' + tagColor + ';">' +
+            tagName
+            + '</div>';
+    return result;
+}
 
 function createStationElement(station) {
     const stationElementId = "station_element_id_" + station.id;
     let favoriteStateImage;
-    if (isStationFavorite(station.id)) {
+    if (isFavorite(station.id)) {
         favoriteStateImage = "img/star_fill.svg";
     } else {
         favoriteStateImage = "img/star_empty.svg";
@@ -69,11 +165,10 @@ function createStationElement(station) {
             '</div>';
     return result;
 }
-;
 
-function isStationFavorite(stationId) {
-    const appState = appStorage.getState();
-    if (appState["favorite"][stationId.toString()]) {
+function isFavorite(stationId) {
+    const appState = APP_STORAGE.getState();
+    if (appState["favorite"].includes(stationId.toString())) {
         return true;
     } else {
         return false;
@@ -81,18 +176,20 @@ function isStationFavorite(stationId) {
 }
 
 function changeFavoriteStatus(stationId) {
-    const appState = appStorage.getState();
-    let isFavorite = appState["favorite"][stationId.toString()];
+    const appState = APP_STORAGE.getState();
+    let favoriteStationIds = appState["favorite"];
     let favoriteStateImage;
-    if (isFavorite) {
-        delete appState["favorite"][stationId.toString()];
+    if (favoriteStationIds.includes(stationId.toString())) {
+        // remove element
+        favoriteStationIds.splice(favoriteStationIds.indexOf(stationId.toString()), 1);
+        appState["favorite"] = favoriteStationIds;
         favoriteStateImage = "img/star_empty.svg";
     } else {
-        appState["favorite"][stationId.toString()] = true;
+        appState["favorite"].push(stationId.toString());
         favoriteStateImage = "img/star_fill.svg";
     }
     $("#station_element_id_" + stationId + " > .station_favorit_star").attr("src", favoriteStateImage);
-    appStorage.setState(appState);
+    APP_STORAGE.setState(appState);
 }
 
 function onClickStationFavorite(event, stationId) {
@@ -158,4 +255,3 @@ function Radio(stations) {
         }
     };
 }
-;
