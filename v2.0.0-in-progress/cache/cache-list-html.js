3b0c9218199a13675fa970b105bc37dd8714fa4a
unsreg <unsreg@gmail.com>
2019-08-05 01:22:52 +0300

    Refactored branch structure

---
+++ v2.0.0-in-progress/cache/cache-list-html.js
@@ -0,0 +1,5 @@
+"use strict";
+
+CACHES["pages"] = [
+    "./offline.html"
+];
---
+++ v2.0.0-in-progress/cache/cache-list-js.js
@@ -0,0 +1,6 @@
+"use strict";
+
+CACHES["scripts"] = [
+    "./js/jquery-3.4.1.min.js",
+    "./js/stations/stations.js"
+];
